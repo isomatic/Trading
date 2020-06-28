8c6c784da59bc5b897ef570e37c443ab1430674c
Chigozie Ezenagu <chigozie.ezenagu@gmail.com>
2020-06-28 16:30:26 -0400

    mvp

--- nnfxrisk/nnfxrisk.js
+++ nnfxrisk/nnfxrisk.js
@@ -14,15 +14,27 @@ function calculate(){
     account = document.querySelector('#account').value;
     atr = document.querySelector('#atr').value;
     price = document.querySelector('#price').value;
-    risk = account * 0.02;
+    baseline = document.querySelector('#baseline').value;
+    risk = Math.round(account * 0.02);
     stopPips = atr * 1.5;
     stopLoss = price - stopPips;
     pipValue = risk / stopPips;
     console.clear();
-    console.log("Stop Loss:" + stopLoss);
-    console.log("Pip Value:" + pipValue);
-    console.log("Account:" + account);
-    console.log("Risk:" + risk);
+    console.log("Stop Loss: " + stopLoss / 10000);
+    console.log("Pip Value: " + pipValue);
+    console.log("Account: " + account);
+    console.log("Risk: " + risk);
+    console.log("Price - Baseline: " + (price - baseline));
+    console.log("ATR: " + atr)
+    console.log("Too Far: " + isTooFar(price, baseline, atr));
 
-    document.querySelector("#pipValue").innerHTML = 8;
+    document.querySelector("#pipValue").innerHTML = pipValue;
 }
+
+function isTooFar(price, baseline, atr) {
+    return Math.abs(price - baseline) > atr;
+}
+
+
+// TODO: Allow only numerical input
+// MT4 indicator quickly highlighting if price is too far from baseline
